2a96eb5fb498e8920be9637379790682b8a3c4d2
Faahffy <tafadzwas294@gmail.com>
2023-03-10 11:08:39 -0800

    libmy

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -3,16 +3,16 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- *
- * Return: void	
+ * Return:void
  */
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
-
 	i = 0;
+
+
 	while (dest[i] != '\0')
 	{
 		i++;
@@ -23,9 +23,8 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
-
-
+	}
 	dest[i] = '\0';
+
 	return (dest);
 }
